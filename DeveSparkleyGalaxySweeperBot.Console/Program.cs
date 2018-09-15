9285dcfffe9d71e77bedfa8f33b1e2138091171b
Devedse <davydavidse@gmail.com>
2018-09-15 15:49:17 +0200

    Initial commit

---
+++ DeveSparkleyGalaxySweeperBot.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeveSparkleyGalaxySweeperBot.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DeveSparkleyGalaxySweeperBot/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DeveSparkleyGalaxySweeperBot
+{
+    public class Class1
+    {
+    }
+}
