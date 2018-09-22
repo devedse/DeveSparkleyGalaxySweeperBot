098ece1f64eedc42af01d07c61c0ae3176dd174b
Ketty Chung <k.chung@vanlanschotkempen.com>
2018-09-22 15:41:36 +0200

    Properties in vakje gestopt

--- DeveSparkleyGalaxySweeperBot/Models/Vakje.cs
+++ DeveSparkleyGalaxySweeperBot/Models/Vakje.cs
@@ -7,9 +7,10 @@ namespace DeveSparkleyGalaxySweeperBot.Models
     class Vakje
     {
         public string Value { get; set; }
+        public int Number => IsNumber ? int.Parse(Value) : -1;
         public bool Revealed => Value != "." && Value != "#";
         public bool IsBomb => Value == "R" || Value == "B";
-        public int Number;
+        public bool IsNumber => Revealed == true && IsBomb == false;
         public List<Vakje> SurroundingVakjes { get; set; }
     }
 }
